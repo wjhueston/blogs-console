5a195552c2e190e3a35e67c9a8e5d4777fd0f6c5
William Hueston <whueston@my.wctc.edu>
2020-03-23 08:29:08 -0500

    Working in theory, might need more testing

--- Program.cs
+++ Program.cs
@@ -15,23 +15,58 @@ namespace BlogsConsole
             {
 
                 // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
+                Console.Write("Blog Manager\n1. Display all blogs\n2. Add Blog\n3. Create Post");
+                
                 var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
                 var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                switch (name)
                 {
-                    Console.WriteLine(item.Name);
+                    case "1":
+                        var blog = new Blog { Name = name };
+
+                        
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                        break;
+                    case "2":
+                        // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
+
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Please select the blog to post to:");
+                        var search = db.Blogs.OrderBy(b => b.Name);
+                        
+                        foreach(var item in search)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                        var select = Console.ReadLine();
+                        foreach(var item in search)
+                        {
+                            if(select == item.Name)
+                            {
+                                Console.WriteLine("Enter the Title of the Post");
+                                var title = Console.ReadLine();
+                                var post = new Post { Title = title };
+                                Console.WriteLine("Enter the content of the Post");
+                                var content = Console.ReadLine();
+                                post.Content = content;
+                                db.AddPost(post);
+                                
+                            }
+                        }
+                        break;
+                    default: break;
                 }
+                
+
+                
             }
             catch (Exception ex)
             {
